8b79e03dae0f5893674ff241b25e567d50664597
ZakharSytoi <zakharsytoi@gmail.com>
2023-06-29 21:55:02 +0200

    Observer pattern task completed

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,17 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
     // Write your code here!
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        RectangleWarehouse.getInstance().put(event.getSource().getId()
+                , new RectangleValues((event.getSource().getSideA() + event.getSource().getSideB()) * 2
+                        , event.getSource().getSideB() * event.getSource().getSideA()));
+    }
 }
