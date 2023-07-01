3517aaff5bb9b13037a36ab8afbbddc34631379a
ZakharSytoi <zakharsytoi@gmail.com>
2023-07-01 09:26:24 +0200

    task 3 completed v1

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,20 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class WordParser extends AbstractTextParser{
 
     // Write your code here!
 
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] arr = string.toCharArray();
+
+        for (char x : arr) {
+            abstractTextComponent.add(new SymbolLeaf(TextComponentType.WORD, x));
+        }
+    }
 }
